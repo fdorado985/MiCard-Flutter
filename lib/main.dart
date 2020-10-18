18c9be36c6dc5005c38fd6f77b64235f33e40183
Juan Dorado <fdorado985@gmail.com>
2020-10-18 17:35:30 -0500

    Add a divider

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,13 @@ class MyApp extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                     fontSize: 20.0),
               ),
+              SizedBox(
+                height: 20.0,
+                width: 200.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
               Card(
                 color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
